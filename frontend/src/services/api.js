b47fa2512bea617c11d427c3760d795bc23c846c
Yann HOUNDJO <yannmgh@gmail.com>
2026-01-12 12:26:40 +0100

    commit déploiement

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://velib-bornes-backend.azurewebsites.net';
 
 // Instance axios avec configuration de base
 const api = axios.create({
